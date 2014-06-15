b372ce95668dc332487360638d199975c6821629
atdaemon <anish.tambe@yahoo.in>
2014-06-15 21:45:07 +0530

    HackerRank weekly 4 prob 1 improve

--- hackerrank/weekly/w4/palindrome_index/prog.py
+++ hackerrank/weekly/w4/palindrome_index/prog.py
@@ -1,12 +1,14 @@
 """
 https://www.hackerrank.com/contests/w4/challenges/palindrome-index
-#v1.0
+#v1.1
+#Fewer comparisions and operations
 """
 
 def isPalindrome(S) :
 	return S==S[::-1]
 
 def removeIndex(S,index) :
+	#print index
 	return S[:index]+S[index+1:]
 
 T = int(raw_input())
@@ -19,7 +21,7 @@ for _ in xrange(T) :
 		print -1
 		continue
 	for index in xrange(N) :
-		if isPalindrome(removeIndex(S,index)) :
+		if S[index] != S[-1*(index+1)] and isPalindrome(removeIndex(S,index)) :
 			print index
 			break
 
