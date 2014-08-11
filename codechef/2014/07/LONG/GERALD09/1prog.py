3bdb0bf64458a5299546d9ac18feb88d36fca116
atdaemon <anish.tambe@yahoo.in>
2014-08-11 08:10:04 +0530

    solved codechef 2014 JUL GERALD09

---
+++ codechef/2014/07/LONG/GERALD09/1prog.py
@@ -0,0 +1,11 @@
+"""
+http://www.codechef.com/JULY14/problems/GERALD09
+#submitting a trivial solution
+"""
+T = int(raw_input())
+for _ in xrange(T) :
+	N,M,K = map(int,raw_input().split())
+	for i in xrange(N) :
+		print 'A'*M
+ 
+
